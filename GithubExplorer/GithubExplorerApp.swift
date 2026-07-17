c9522da51ef037ca59e768e13996d2ef2f8451bc
Hammad Ahmed <hammad.ahmed@m3tech.com.pk>
2026-07-17 16:47:14 +0500

    initial commit

---
+++ GithubExplorer/GithubExplorerApp.swift
@@ -0,0 +1,17 @@
+//
+//  GithubExplorerApp.swift
+//  GithubExplorer
+//
+//  Created by Hammad Ahmed on 17/07/2026.
+//
+
+import SwiftUI
+
+@main
+struct GithubExplorerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
